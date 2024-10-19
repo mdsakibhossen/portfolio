1e96296cb7c6ec5c5f1f360ffd6584a1daa57b5c
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-10-19 14:25:44 +0600

    Portfolio Section of home page is Completed

--- src/components/portfolio-card/PortfolioCard.jsx
+++ src/components/portfolio-card/PortfolioCard.jsx
@@ -1,73 +1,160 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+// import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./portfolio-card.css";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+
+const parentVariants = {
+  hidden: { y: "100%", opacity: 0 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      staggerChildren: 0.1,
+      when: "beforeChildren",
+      type: "spring",
+      stiffness: 300,
+      damping: 20,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { opacity: 0, y: "100%" },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+      damping: 20,
+    },
+  },
+};
 const PortfolioCard = ({ portfolio }) => {
-  const { image, title, description, skills, liveDemo, sourceCode } = portfolio;
-
+  const { images, title, description, skills } = portfolio;
+  const ref = useRef(null);
+  const isInView = useInView(ref);
   return (
-    <div className="portfolio-card w-full h-auto group rounded-xl overflow-hidden shadow-lg transition-all hover:shadow-2xl duration-500 bg-white">
-      {/* Image container */}
-      <div className="img-box w-full h-[300px] relative overflow-hidden">
-        <Link href={""}>
-          <Image
-            src={image}
-            alt={title}
-            width={1000}
-            height={600}
-            className="absolute top-0 left-0 h-auto min-h-full w-full object-top transition-transform duration-1000 ease-in-out group-hover:translate-y-[calc(-100%_+_300px)]"
-            priority
-          />
-        </Link>
-      </div>
+    <motion.div
+      ref={ref}
+      variants={parentVariants}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      // className="overflow-hidden"
+    >
+      <div className="portfolio-card w-full h-auto group rounded-xl overflow-hidden shadow-lg transition-all hover:shadow-2xl duration-500 bg-white">
+        {/* Image container */}
 
-      {/* Content */}
-      <div className="content p-4">
-        {/* Title */}
-        <h3 className="text-lg font-semibold text-gray-800 mb-2 group-hover:text-secondary-400 transition-colors duration-300">
-          {title}
-        </h3>
-
-        {/* Description */}
-        <p className="text-sm text-gray-600 leading-relaxed mb-4">
-          {description}
-        </p>
-
-        {/* Skills */}
-        <div className="flex flex-wrap gap-2 mb-4">
-          {skills.map((skill, index) => (
-            <span
-              key={index}
-              className="text-xs font-medium bg-secondary-100 text-secondary-400 px-3 py-1 rounded-full"
-            >
-              {skill}
-            </span>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          {images.map((image, i) => (
+            <SwiperSlide key={i}>
+              <div className="img-box w-full h-[300px] relative overflow-hidden">
+                <Link href={""}>
+                  <Image
+                    src={image}
+                    alt={title}
+                    width={1000}
+                    height={600}
+                    className="absolute top-0 left-0 h-auto min-h-full w-full object-top transition-transform duration-1000 ease-in-out group-hover:translate-y-[calc(-100%_+_300px)]"
+                    priority
+                  />
+                </Link>{" "}
+              </div>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
 
-        {/* Buttons */}
-        <div className="flex gap-4 flex-wrap">
-          {/* Live Demo Button */}
-          <Link
-            href={""}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block px-5 py-2 text-sm font-semibold bg-primary-400 text-white rounded-full shadow-lg hover:bg-primary-500 transition-all duration-300 transform hover:scale-105"
+        {/* Content */}
+        <div className="content p-4">
+          {/* Title */}
+          <motion.h3
+            variants={childVariants}
+            className="text-lg font-semibold text-gray-800 mb-2 group-hover:text-secondary-400 transition-colors duration-300"
           >
-            Live Demo
-          </Link>
+            {title}
+          </motion.h3>
 
-          {/* Source Code Button */}
-          <Link
-            href={""}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block px-5 py-2 text-sm font-semibold bg-secondary-400 text-white rounded-full shadow-lg hover:bg-secondary-500 transition-all duration-300 transform hover:scale-105"
+          {/* Description */}
+          <motion.p
+            variants={childVariants}
+            className="text-sm text-gray-600 leading-relaxed mb-4"
           >
-            Source Code
-          </Link>
+            {description}
+          </motion.p>
+
+          {/* Skills */}
+          <div className="flex flex-wrap gap-2 mb-4">
+            {skills.map((skill, index) => (
+              <motion.span
+                key={index}
+                variants={childVariants}
+                className="text-xs font-medium bg-secondary-100 text-secondary-400 px-3 py-1 rounded-full inline-block"
+              >
+                {skill}
+              </motion.span>
+            ))}
+          </div>
+
+          {/* Buttons */}
+          <div className="flex gap-4 flex-wrap">
+            {/* Live Demo Button */}
+            <motion.div
+              variants={childVariants}
+              whileHover={{
+                scale: 1.15,
+                transition: { type: "spring", stiffness: 300 },
+              }}
+            >
+              <Link
+                href={""}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block px-5 py-2 text-sm font-semibold bg-primary-400 text-white rounded-full shadow-lg hover:bg-primary-500 transition-all duration-300 transform hover:scale-105"
+              >
+                Live Demo
+              </Link>
+            </motion.div>
+
+            {/* Source Code Button */}
+            <motion.div
+              variants={childVariants}
+              whileHover={{
+                scale: 1.15,
+                transition: { type: "spring", stiffness: 300 },
+              }}
+            >
+              <Link
+                href={""}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block px-5 py-2 text-sm font-semibold bg-secondary-400 text-white rounded-full shadow-lg hover:bg-secondary-500 transition-all duration-300 transform hover:scale-105"
+              >
+                Source Code
+              </Link>
+            </motion.div>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/lib/portfolios.js
+++ src/lib/portfolios.js
@@ -1,36 +1,36 @@
-import P1Image from "../../public/images/1.png";
+import P1Image from "../../public/images/1.png"
 export const portfolios = [
     {
         id: 1,
-        image: P1Image,
+        images: [P1Image, P1Image, P1Image],
         title: "Personal Portfolio",
         description: "I specialize in creating dynamic, responsive websites and web applications with a passion for clean code and seamless user experiences. Let's build something amazing together!",
-        skills: ["HTML","CSS","JS"]
+        skills: ["HTML", "CSS", "JS"]
     },
     {
         id: 2,
-        image: P1Image,
+        images: [P1Image, P1Image, P1Image],
         title: "Personal Portfolio",
         description: "I specialize in creating dynamic, responsive websites and web applications with a passion for clean code and seamless user experiences. Let's build something amazing together!",
         skills: ["HTML", "CSS", "JS"]
     },
     {
         id: 3,
-        image: P1Image,
+        images: [P1Image, P1Image, P1Image],
         title: "Personal Portfolio",
         description: "I specialize in creating dynamic, responsive websites and web applications with a passion for clean code and seamless user experiences. Let's build something amazing together!",
-        skills: ["HTML","CSS","JS"]
+        skills: ["HTML", "CSS", "JS"]
     },
     {
         id: 4,
-        image: P1Image,
+        images: [P1Image, P1Image, P1Image],
         title: "Personal Portfolio",
         description: "I specialize in creating dynamic, responsive websites and web applications with a passion for clean code and seamless user experiences. Let's build something amazing together!",
-        skills: ["HTML","CSS","JS"]
+        skills: ["HTML", "CSS", "JS"]
     },
     {
         id: 5,
-        image: P1Image,
+        images: [P1Image, P1Image, P1Image],
         title: "Personal Portfolio",
         description: "I specialize in creating dynamic, responsive websites and web applications with a passion for clean code and seamless user experiences. Let's build something amazing together!",
         skills: ["HTML", "CSS", "JS"]
