e777cb26cc28455b5186b339d5688ebb0a61a93b
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-10-26 23:34:21 +0600

    Updated: portfolio section of portfolio page

--- src/components/portfolio-page/portfolio/Portfolio.jsx
+++ src/components/portfolio-page/portfolio/Portfolio.jsx
@@ -1,11 +1,37 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import SecContainer from "@/components/sec-container/SecContainer";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useInView } from "framer-motion";
 import { categories, portfolios } from "@/lib/portfolios";
 import PortfolioCard from "@/components/portfolio-card/PortfolioCard";
+const parentVariants = {
+  hidden: { y: 150, opacity: 0 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      staggerChildren: 0.1,
+      when: "beforeChildren",
+      type: "spring",
+      stiffness: 300,
+      damping: 20,
+    },
+  },
+};
 
+const childVariants = {
+  hidden: { opacity: 0, y: "100%" },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 300,
+      damping: 20,
+    },
+  },
+};
 const Portfolio = () => {
   const [activeCategory, setActiveCategory] = useState("all");
   const [activePortfolios, setActivePortfolios] = useState(portfolios);
@@ -29,70 +55,88 @@ const Portfolio = () => {
     active: { scale: 1.1, transition: { duration: 0.2 } },
   };
 
+  const catRef = useRef(null);
+  const isCatInView = useInView(catRef, { amount: 0.2 });
+
   return (
     <SecContainer className={"py-20"}>
-      {/* Category Tabs for Desktop */}
-      <div className="hidden md:flex flex-wrap justify-center gap-4 mb-8">
-        <motion.button
-          variants={tabVariants}
-          initial="initial"
-          whileHover="hover"
-          className={`${
-            activeCategory === "all"
-              ? "text-primary-500 border-b-2 border-primary-500"
-              : "text-gray-500"
-          } pb-2 transition-colors duration-300`}
-          onClick={() => setActiveCategory("all")}
-          animate={activeCategory === "all" ? "active" : "initial"}
-        >
-          All
-        </motion.button>
-        {categories.map((category) => (
-          <motion.button
-            key={category.id}
-            variants={tabVariants}
-            initial="initial"
-            whileHover="hover"
-            className={`${
-              activeCategory === category.id
-                ? "text-primary-500 border-b-2 border-primary-500"
-                : "text-gray-500"
-            } pb-2 transition-colors duration-300`}
-            onClick={() => setActiveCategory(category.id)}
-            animate={activeCategory === category.id ? "active" : "initial"}
-          >
-            {category.title}
-          </motion.button>
-        ))}
-      </div>
-
-      {/* Select Box for Mobile Devices */}
-      <div className="md:hidden mb-8">
-        <select
-          value={activeCategory}
-          onChange={(e) => setActiveCategory(e.target.value)}
-          className="block w-full border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-primary-400"
-        >
-          <option value="all">All</option>
+      <motion.div
+        variants={parentVariants}
+        initial="hidden"
+        animate={isCatInView ? "visible" : "hidden"}
+        ref={catRef}
+        className="categories"
+      >
+        {/* Category Tabs for Desktop */}
+        <div className="hidden lg:flex flex-wrap justify-center gap-6 mb-8">
+          <motion.div variants={childVariants}>
+            <motion.button
+              variants={tabVariants}
+              initial="initial"
+              whileHover="hover"
+              className={`${
+                activeCategory === "all"
+                  ? "text-secondary-400 border-b-2 border-secondary-400"
+                  : "text-gray-500"
+              } pb-2 transition-colors duration-300`}
+              onClick={() => setActiveCategory("all")}
+              animate={activeCategory === "all" ? "active" : "initial"}
+            >
+              All
+            </motion.button>
+          </motion.div>
           {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.title}
-            </option>
+            <motion.div key={category.id} variants={childVariants}>
+              <motion.button
+                // key={category.id}
+                variants={tabVariants}
+                initial="initial"
+                whileHover="hover"
+                className={`${
+                  activeCategory === category.id
+                    ? "text-secondary-400 border-b-2 border-secondary-400"
+                    : "text-gray-500"
+                } pb-2 transition-colors duration-300`}
+                onClick={() => setActiveCategory(category.id)}
+                animate={activeCategory === category.id ? "active" : "initial"}
+              >
+                {category.title}
+              </motion.button>
+            </motion.div>
           ))}
-        </select>
-      </div>
+        </div>
+
+        {/* Select Box for Mobile Devices */}
+        <motion.div
+          variants={childVariants}
+          className="lg:hidden mb-8 max-w-[600px] mx-auto"
+        >
+          <select
+            value={activeCategory}
+            onChange={(e) => setActiveCategory(e.target.value)}
+            className="block w-full border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-primary-400"
+          >
+            <option value="all">All</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.title}
+              </option>
+            ))}
+          </select>
+        </motion.div>
+      </motion.div>
 
       {/* Portfolios or No Portfolios Message */}
       <AnimatePresence>
         {activePortfolios.length === 0 ? (
           <motion.div
             key="no-portfolios"
-            className="text-center text-gray-500 py-10"
+            className="text-center text-gray-500 py-10 text-2xl md:text-4xl"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            No portfolios are available.
+            No Portfolios are Available.
           </motion.div>
         ) : (
           <motion.div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
